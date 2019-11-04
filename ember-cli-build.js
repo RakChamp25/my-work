2279ea7f3c3505a7416f1c45aeb84e0387f43746
ajithkannan.r <ajithkannan.r@zohocorp.com>
2019-11-04 16:18:46 +0530

    Optional chaining

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    babel: {
+      plugins: [require('ember-auto-import/babel-plugin'), '@babel/plugin-proposal-optional-chaining']
+    }
     // Add options here
   });
 
